8f8ff299d194c60313686737064d516b2f5984b0
fam-system <famsystemwork@gmail.com>
2025-06-18 14:36:11 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+/*const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'admin', // tu contraseña
   database: 'tpiprog3', // el nombre de tu base
   port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});*/
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS, // tu contraseña
+  database: process.env.DB_NAME, // el nombre de tu base
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
